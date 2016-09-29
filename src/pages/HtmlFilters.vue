2fc8d0d008f6aeb8a2e741f8dd5f97e60222937d
savroff <savroff@gmail.com>
2016-09-29 10:47:30 -0400

    Add basic HTML tags

--- src/pages/HtmlFilters.vue
+++ src/pages/HtmlFilters.vue
@@ -1,19 +1,69 @@
 <template lang="">
   <div class="article">
-    <h1 class="main-title">Money Filters</h1>
-    <p>Money filters format prices based on the Currency choosed in
-      <a href="/admin/payments/edit">Payments Settings</a>.</p>
+    <h1 class="main-title">HTML Filters</h1>
+    <p>HTML filters wrap assets in HTML tags.
     <hr>
-    <h2>money_with_currency</h2>
+    <h2>image_tag</h2>
     <p>
-      Formats the price based on the store HTML with currency setting.
+      Generates an image tag.
     </p>
     <pre>
       <code class="liquid"  v-el:liqud1>
-    {{{'{{ 145 | money_with_currency }}'}}}
+    {{{'{{ program.horizontal_image.small }}'}}}
       </code>
-      <code class="html"  v-el:liqud2>
-    $1.45
+    </pre>
+    <p>
+      Filter accepts parameters to output an alt tag, class names and styles.
+    </p>
+    <pre>
+      <code class="liquid"  v-el:liqud2>
+    {{{'{{ program.horizontal_image.small | image_tag, style: "border: 1px;", class: "my-image", alt: "best image" }}'}}}
+      </code>
+      <code class="html"  v-el:liqud3>
+    &lt;img style="border: 1px;" class="my-image" alt="best image" src="/assets/img.png">
+      </code>
+    </pre>
+    <hr>
+    <h2>javascript_tag</h2>
+    <p>
+      Generates a JavaScript script tag.
+    </p>
+    <p>
+      For the inside theme assets you can just put a name of file.
+    </p>
+    <pre>
+      <code class="liquid"  v-el:liqud4>
+    {{{'{{ "main.js" | javascript_tag }}'}}}
+      </code>
+    </pre>
+    <p>
+      For the outside theme assets you need to put a full name and left <code>//</code> to prevent cross-origin requests.
+      Please make sure you put links to minified version.
+    </p>
+    <pre>
+      <code class="liquid"  v-el:liqud5>
+    {{{'{{ "//cdn.js.net/1.5.9/slick.min.js" | javascript_tag }}'}}}
+      </code>
+    </pre>
+    <hr>
+    <h2>stylesheet_tag</h2>
+    <p>
+      Generates a stylesheet tag.
+    </p>
+    <p>
+      For the inside theme assets you can just put a name of file.
+    </p>
+    <pre>
+      <code class="liquid"  v-el:liqud6>
+    {{{'{{ "main.css" | stylesheet_tag }}'}}}
+      </code>
+    </pre>
+    <p>
+      For the outside theme assets you need to put a full name and left <code>//</code> to prevent cross-origin requests.
+    </p>
+    <pre>
+      <code class="liquid"  v-el:liqud7>
+    {{{'{{ "//cdn.css.net/1.5.9/slick.css" | stylesheet_tag }}'}}}
       </code>
     </pre>
   </div>
